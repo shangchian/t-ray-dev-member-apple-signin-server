0f6f9c1797330e630fc5425dc37837100e5cf47a
Timm Preetz <timm@preetz.us>
2020-04-25 15:53:22 +0200

    Add Android redirect endpoint as well as session creation endpoint

--- server.js
+++ server.js
@@ -5,13 +5,8 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const AppleAuth = require('apple-auth');
+const jwt = require('jsonwebtoken');
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -22,11 +17,44 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
+// The callback route used for Android, which will send the callback parameters from Apple into the Android app.
+// This is done using a deeplink, which will cause the Chrome Custom Tab to be dismissed and providing the parameters from Apple back to the app.
 app.get("/callbacks/sign_in_with_apple", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  const redirect = `intent://callback?${new URLSearchParams(req.body).toString()}#Intent;package=${process.env.ANDROID_PACKAGE_IDENTIFIER};scheme=signinwithapple;end`;
+
+  console.log('Redirecting to ${redirect}');
+
+  response.redirect(307, redirect);
 });
+
+// Endpoint for the app to login or register with the `code` obtained during Sign in with Apple
+//
+// Use this endpoint to exchange the code (which must be validated with Apple within 5 minutes) for a session in your system
+app.post("/sign_in_with_apple", async (request, response) => {
+  const auth = new AppleAuth({
+    client_id: process.env.CLIENT_ID,
+    team_id: process.env.TEAM_ID,
+    redirect_uri: '', // does not matter here, as this is already the callback that verifies the token after the redirection
+    key_id: process.env.KEY_ID,
+  }, process.env.KEY_CONTENTS, 'text');
+
+  const accessToken = await auth.accesToken(request.params.code);
+
+  const idToken = jwt.decode(accessToken.id_token);
+
+  const userID = idToken.sub;
+
+  // `userEmail` and `userName` will only be provided for the initial authorization with your app
+  const userEmail = idToken.email;
+  const { name: userName } = JSON.parse(req.body.user);
+
+  // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
+  const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
+
+  console.log(`sessionID = ${sessionID}`);
+
+  response.json({ "sessionId": sessionID });
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
