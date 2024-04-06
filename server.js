c786ed50a43a5a2191c239abfda049723b90ceaf
Glitch (flutter-sign-in-with-apple-example) <none>
2024-04-06 07:12:15 +0000

    🌟🐜 Checkpoint ./server.js:731924/286

--- server.js
+++ server.js
@@ -10,15 +10,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
 // The callback route used for Android, which will send the callback parameters from Apple into the Android app.
 // This is done using a deeplink, which will cause the Chrome Custom Tab to be dismissed and providing the parameters from Apple back to the app.
 app.post("/callbacks/sign_in_with_apple", (request, response) => {
